222b6a9f93fe1a8b8ff101fe4948c971f033ba47
nishanthprabhu2 <nishanthprabhu26@gmail.com>
2019-07-17 10:16:22 +0530

    13th solution

--- Algo.js
+++ Algo.js
@@ -177,4 +177,22 @@ function titleCase(str) {
 
 titleCase("I'm a little tea pot");
 
+//-----------------------------------------------------------------
+//13
+//Slice and Splice You are given two arrays and an index.
+// Use the array methods slice and splice to copy each element of the first array into the second array, in order.
+// Begin inserting elements at index n of the second array.
+// Return the resulting array. The input arrays should remain the same after the function runs.
+function frankenSplice(arr1, arr2, n) {
+    // It's alive. It's alive!
+    let res = arr2.slice();
+    for(var i=0;i<arr1.length;i++){
+      res.splice(n,0,arr1[i]);
+      n++;
+    }
+    return res;
+  }
+  
+  frankenSplice([1, 2, 3], [4, 5], 1);
+
 
